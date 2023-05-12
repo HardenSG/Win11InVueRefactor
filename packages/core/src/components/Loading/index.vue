0ce49eddec14e129b9761d0034c257653d916d1c
SG <2767525216@qq.com>
2023-05-12 09:39:48 +0800

    chore: do somethings for vercel

--- packages/core/src/components/Loading/index.vue
+++ packages/core/src/components/Loading/index.vue
@@ -1,17 +1,14 @@
 <template>
-    <h1>
-        Loading.....
-    </h1>
+  <h1>Loading.....</h1>
 </template>
 
 <style lang="scss" scoped>
-    h1 {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100vw;
-        height: 100vh;
-        transition: all 0.5s;
-    }
+h1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  transition: all 0.5s;
+}
 </style>
-
